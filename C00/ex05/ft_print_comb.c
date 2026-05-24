6e14cb91e1068d1fe093f89140b3e814475e2b92
aladin766 <bslh1002@gmail.com>
2026-05-24 13:14:23 +0200

    push

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+void ft_print_comb(void)
+{
+    char a = '0';
+    char b = '1';
+    char c = '2';
+    char e = ',';
+    char f = ' ';
+    while (a <= '7')
+    {
+        while (b <= '8')
+        {
+            while (c <= '9')
+            {
+                write(1, &a, 1);
+                write(1, &b, 1);
+                write(1, &c, 1);
+                if (a == '7' && b == '8' && c == '9')
+                {}
+                else {
+                    write(1, &e, 1);
+                    write(1, &f, 1);
+                }
+                c++;
+            }
+            c = '2';
+            c += 1;
+            b++;
+        }
+        b = '2';
+        b += 1;
+        a++;
+    }
+}
+
+int main (void)
+{
+    ft_print_comb();
+    return(0);
+}
