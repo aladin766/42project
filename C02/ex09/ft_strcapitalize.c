fa65d8ed5f3d46ea9f84a1bc88d6b2e151b87eda
aladin766 <bslh1002@gmail.com>
2026-06-19 03:47:15 +0200

    push

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -3,16 +3,25 @@ char    *ft_strcapitalize(char *str)
     int i;
     i = 0;
 
-    while (str[i])
+    while (str[i] != '\0')
     {
         if (str[i] >= 65 && str[i] <= 90)
         {
-            i += 32;
+            str[i] += 32;
         }
+        i++;
         if (str[i] >= 97 && str[i] <= 122)
         {
-            i -= 32;
+            str[i] -= 32;
         }
         i++;
     }
+    return(str);
+}
+#include <stdio.h>
+int main()
+{
+    char str[] = "HelIO";
+    printf("\"%s\"", ft_strcapitalize(str));
+    return(0);
 }
