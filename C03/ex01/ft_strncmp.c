f831a9ea04ddd93d66e2ede9ba448f89d0c67cb2
aladin766 <bslh1002@gmail.com>
2026-06-23 03:24:11 +0200

    push

---
+++ C03/ex01/ft_strncmp.c
@@ -0,0 +1,22 @@
+int ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+    unsigned int i;
+    i = 0;
+
+    while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n)
+    {
+        i++;
+    }
+    return (s1[i] - s2[i]);
+}
+
+#include <stdio.h>
+int main()
+{
+    char s1[] = "loletzz";
+    char s2[] = "lolete";
+    int n = 5;
+
+    printf("diff = %d", ft_strncmp(s1, s2, n));
+    return(0);
+}
